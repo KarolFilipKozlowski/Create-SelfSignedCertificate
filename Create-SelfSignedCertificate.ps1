1a134b427bb1b1161a7bab1bfeae83182d72ef00
Karol Kozłowski <ravenweb@gmail.com>
2022-12-18 16:46:17 +0100

    Update Create-SelfSignedCertificate.ps1

--- Create-SelfSignedCertificate.ps1
+++ Create-SelfSignedCertificate.ps1
@@ -1,40 +1,26 @@
 #Requires -RunAsAdministrator
-<#
-.SYNOPSIS
-Creates a Self Signed Certificate for use in server to server authentication
-.DESCRIPTION
-.EXAMPLE
-.\Create-SelfSignedCertificate.ps1 -CommonName "MyCert" -StartDate 2015-11-21 -EndDate 2017-11-21
-This will create a new self signed certificate with the common name "CN=MyCert". During creation you will be asked to provide a password to protect the private key.
-.EXAMPLE
-.\Create-SelfSignedCertificate.ps1 -CommonName "MyCert" -StartDate 2015-11-21 -EndDate 2017-11-21 -Password (ConvertTo-SecureString -String "MyPassword" -AsPlainText -Force)
-This will create a new self signed certificate with the common name "CN=MyCert". The password as specified in the Password parameter will be used to protect the private key
-.EXAMPLE
-.\Create-SelfSignedCertificate.ps1 -CommonName "MyCert" -StartDate 2015-11-21 -EndDate 2017-11-21 -Force
-This will create a new self signed certificate with the common name "CN=MyCert". During creation you will be asked to provide a password to protect the private key. If there is already a certificate with the common name you specified, it will be removed first.
-#>
-Param(
+<# Creates a Self Signed Certificate for use in server to server authentication #>
+function CreateSelfSignedCertificate {
+    Param(
+    [Parameter(Mandatory=$true)]
+    [string]$CommonName,
 
-   [Parameter(Mandatory=$true)]
-   [string]$CommonName,
+    [Parameter(Mandatory=$true)]
+    [DateTime]$StartDate,
 
-   [Parameter(Mandatory=$true)]
-   [DateTime]$StartDate,
+    [Parameter(Mandatory=$true)]
+    [DateTime]$EndDate,
 
-   [Parameter(Mandatory=$true)]
-   [DateTime]$EndDate,
+    [Parameter(Mandatory=$true)]
+    [SecureString]$Password,
 
-   [Parameter(Mandatory=$false, HelpMessage="Will overwrite existing certificates")]
-   [Switch]$Force,
+    [Parameter(Mandatory=$true)]
+    [bool]$RemoveCert,
 
-   [Parameter(Mandatory=$false)]
-   [SecureString]$Password
+    [Parameter(Mandatory=$true)]
+    [bool]$Overwrite
 )
-
-# DO NOT MODIFY BELOW
-
-function CreateSelfSignedCertificate(){
-
+    
     #Remove and existing certificates with the same common name from personal and root stores
     #Need to be very wary of this as could break something
     if($CommonName.ToLower().StartsWith("cn="))
@@ -43,17 +29,16 @@ function CreateSelfSignedCertificate(){
         $CommonName = $CommonName.Substring(3)
     }
     $certs = Get-ChildItem -Path Cert:\LocalMachine\my | Where-Object{$_.Subject -eq "CN=$CommonName"}
-    if($certs -ne $null -and $certs.Length -gt 0)
+    if($null -ne $certs -and $certs.Length -gt 0)
     {
-        if($Force)
+        if($Overwrite)
         {
-
             foreach($c in $certs)
             {
                 remove-item $c.PSPath
             }
         } else {
-            Write-Host -ForegroundColor Red "One or more certificates with the same common name (CN=$CommonName) are already located in the local certificate store. Use -Force to remove them";
+            Write-Host -ForegroundColor Red "One or more certificates with the same common name (CN=$CommonName) are already located in the local certificate store. Use -Overwrite to remove them";
             return $false
         }
     }
@@ -90,43 +75,94 @@ function CreateSelfSignedCertificate(){
     $enrollment.InitializeFromRequest($cert)
     $certdata = $enrollment.CreateRequest(0)
     $enrollment.InstallResponse(2, $certdata, 0, "")
-    return $true
-}
-
-function ExportPFXFile()
-{
+    
     if($CommonName.ToLower().StartsWith("cn="))
     {
         # Remove CN from common name
         $CommonName = $CommonName.Substring(3)
-    }
-    if($Password -eq $null)
-    {
-        $Password = Read-Host -Prompt "Enter Password to protect private key" -AsSecureString
     }
     $cert = Get-ChildItem -Path Cert:\LocalMachine\my | where-object{$_.Subject -eq "CN=$CommonName"}
 
-    Export-PfxCertificate -Cert $cert -Password $Password -FilePath "$($CommonName).pfx"
-    Export-Certificate -Cert $cert -Type CERT -FilePath "$CommonName.cer"
+    $_ExportPfxCertificate = Export-PfxCertificate -Cert $cert -Password $Password -FilePath "$($CommonName).pfx"
+    $_ExportCertificate = Export-Certificate -Cert $cert -Type CERT -FilePath "$CommonName.cer"
+
+    Write-Host "Certificates: " -ForegroundColor Green
+    Write-Host "- $($CommonName).pfx" -ForegroundColor Green
+    Write-Host "- $($CommonName).cer" -ForegroundColor Green
+    Write-Host "expoted to the $($PSScriptRoot) folder." -ForegroundColor Green
+
+    if($RemoveCert)
+    {
+        # Once the certificates have been been exported we can safely remove them from the store
+        if($CommonName.ToLower().StartsWith("cn="))
+        {
+            # Remove CN from common name
+            $CommonName = $CommonName.Substring(3)
+        }
+        $certs = Get-ChildItem -Path Cert:\LocalMachine\my | Where-Object{$_.Subject -eq "CN=$CommonName"}
+        foreach($c in $certs)
+        {
+            remove-item $c.PSPath
+        }
+    }
 }
 
-function RemoveCertsFromStore()
-{
-    # Once the certificates have been been exported we can safely remove them from the store
-    if($CommonName.ToLower().StartsWith("cn="))
-    {
-        # Remove CN from common name
-        $CommonName = $CommonName.Substring(3)
-    }
-    $certs = Get-ChildItem -Path Cert:\LocalMachine\my | Where-Object{$_.Subject -eq "CN=$CommonName"}
-    foreach($c in $certs)
-    {
-        remove-item $c.PSPath
-    }
+$date = Get-Date
+# -CommonName
+$CommonName = $Null
+While (($Null -eq $CommonName) -or ($CommonName.Length -lt 6)) {
+    $CommonName = Read-Host -Prompt "Enter common name for certificate (min. 5 chars)"
 }
-
-if(CreateSelfSignedCertificate)
-{
-    ExportPFXFile
-    RemoveCertsFromStore
+# -StartDate
+$defaultStartDate = $date.ToString("yyyy-MM-dd")
+Write-Host "Press enter to accept the default start date - " -NoNewline
+Write-Host "$($defaultStartDate)" -ForegroundColor Yellow -NoNewline
+Write-Host " or enter custom date: " -NoNewline
+$StartDate = Read-Host
+$StartDate = ($defaultStartDate, $StartDate)[[bool]$tStartDate]
+# -EndDate
+$defaultEndDate = $date.AddYears(2).ToString("yyyy-MM-dd")
+Write-Host "Press enter to accept the default end date - " -NoNewline
+Write-Host "$($defaultEndDate)" -ForegroundColor Yellow -NoNewline
+Write-Host " or enter custom date: " -NoNewline
+$EndDate = Read-Host
+$EndDate = ($defaultEndDate, $EndDate)[[bool]$EndDate]
+# -Password
+[SecureString]$Password = $Null
+While (($Null -eq $Password) -or ($Password.Length -lt 6)) {
+    $Password = Read-Host -Prompt "Enter password to protect private key for certificate (min. 5 chars)" -AsSecureString
 }
+# -RemoveCert
+[bool]$RemoveCert = $false
+$defaultRemoveCert = $Null
+While (($Null -eq $defaultRemoveCert) -and (($defaultRemoveCert -ne "Y") -or ($defaultRemoveCert -ne "N"))) {
+    Write-Host "Remove certificate from computer? " -NoNewline
+    Write-Host "Y for Yes" -ForegroundColor Yellow -NoNewline
+    Write-Host " or " -NoNewline
+    Write-Host "N for No (default)" -ForegroundColor Yellow -NoNewline
+    Write-Host ": " -NoNewline
+    $defaultRemoveCert = Read-Host
+    $defaultRemoveCert = ("N", $defaultRemoveCert)[[bool]$defaultRemoveCert]
+}
+if($defaultRemoveCert -eq "Y")
+{
+    $RemoveCert = $true
+}
+# -Overwrite
+[bool]$Overwrite = $true
+$defaultOverwrite = $Null
+While (($Null -eq $defaultOverwrite) -and (($defaultOverwrite -ne "Y") -or ($defaultOverwrite -ne "N"))) {
+    Write-Host "Overwrite existing certificates? " -NoNewline
+    Write-Host "Y for Yes (default)" -ForegroundColor Yellow -NoNewline
+    Write-Host " or " -NoNewline
+    Write-Host "N for No" -ForegroundColor Yellow -NoNewline
+    Write-Host ": " -NoNewline
+    $defaultOverwrite = Read-Host
+    $defaultOverwrite = ("N", $defaultOverwrite)[[bool]$defaultOverwrite]
+}
+if($defaultForce -eq "N")
+{
+    $Overwrite = $false
+}
+<# RUN #>
+CreateSelfSignedCertificate -CommonName $CommonName -StartDate $StartDate -EndDate $EndDate -Password $Password -RemoveCert $RemoveCert -Overwrite $Overwrite
